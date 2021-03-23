048c1ae012683c322350f93d62d4ba2fae588e22
Maria Hayward <hayward.maria@gmail.com>
2021-03-23 10:39:13 -0300

    subo tracking de usuarios

--- trackingUsuarios.js
+++ trackingUsuarios.js
@@ -1,19 +1,30 @@
-let apiUrl = "https://www.clarin.com/"
-let data = {
-    userAgent: 'Mozila',
-    id: '1'
+let fingerprintjs;
+function initFingerprintJS() {
+    FingerprintJS.load().then(fp => {
+        // The FingerprintJS agent is ready.
+        // Get a visitor identifier when you'd like to.
+        fp.get().then(result => {
+            // This is the visitor identifier:
+            const visitorId = result.visitorId;
+            console.log(visitorId);
+            fingerprints = visitorId
+        });
+    });
 }
-fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-        'Content-type': 'text/json'
-    },
-    body: JSON.stringify(data)
-})
-    .then(response =>
-        response.json()
-    )
-    .then(data => {
-        console.log('ver que trae data')
-        console.log(data)
-    })
+
+let referrer = document.referrer;
+let useragent = navigator.userAgent;
+let current_page_name = window.location.href;
+let page_source = document.referrer;
+initFingerprintJS();
+
+const postTrackUser = async () => {
+    try {
+        const resPost = await axios.post(urlcualquieraquenotienequeexistir, { useragent, referrer, fingerprints, page_source })
+        //const post = await resPost.json()
+        const postData = resPost.data;
+        console.log(postData)
+    } catch (err) {
+        console.log(err);
+    }
+}
